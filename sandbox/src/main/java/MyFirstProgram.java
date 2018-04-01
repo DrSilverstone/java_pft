f1be44a766f6a274b46734f13169b6e3f4bb431e
Unknown <olyaser@mail.ru>
2018-04-01 15:06:37 +0300

    ones agan

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,9 +1,8 @@
-/**
- * Created by aurinko on 30/3/18.
- */
-public class MyFirstProgram {
+public class MyFirstProgram{
 
-    public static void main(String[] args){
-        System.out.println("Hello, world");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+
+	}
 }
+
