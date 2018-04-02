e720d84a772e6b74ee70fcd18fcdaf7443ca6014
aurinko <aurinko@aurinkos-MacBook-Pro.local>
2018-04-02 14:33:08 +0300

    task 2.02
    исправлено

--- sandbox/src/main/java/ru/stqua/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqua/pft/sandbox/Point.java
@@ -9,8 +9,8 @@ public class Point {
         this.y = y;
     }
 
-    public static double distance(double x1, double y1, double x2, double y2){
-        return Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+    public double distance(Point p2){
+        return Math.sqrt(Math.pow((p2.x - this.x),2) + Math.pow(p2.y - this.y,2));
     }
 
 
