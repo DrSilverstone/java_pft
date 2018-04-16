af027479980b0e55214c846ec5f6c960689a1a6f
Unknown <olyaser@mail.ru>
2018-04-16 19:17:28 +0300

    реализована проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,11 +1,19 @@
 package ru.stqua.pft.addressbook.tests;
 import org.testng.annotations.Test;
+import ru.stqua.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase{
     
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+
+        if (! app.getGroupHelper().isThereAGroup()){
+
+            // if we wont to check posibility to delite group, but no group exist, I'm create the group
+
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
